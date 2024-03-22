2badf86c923f8e7701db05f8acd4ff7f0751c3d9
GL-Gabriel-Lincoln <linco.oficial@gmail.com>
2024-03-21 22:10:47 -0300

    .

---
+++ Soma.js
@@ -0,0 +1,32 @@
+// Desenvolva o código em JavaScript de um programa que soma números inteiros informados 
+// pelo usuário até que um número negativo seja informado e então imprime o resultado da 
+// soma na tela.
+
+function clearResult() {
+    document.getElementById('result').innerHTML = '';
+    document.getElementById('result').style.display = 'none';
+}
+
+function calculateSum() {
+    
+    let x = 0;
+    let sum = 0;
+    let i = 1;
+    
+    while (x >= 0) {
+
+        x = prompt("Digite o(s) valor(es) que somar com outro(s) valor(es): ")
+
+        x = parseInt(x);
+
+        if(x < 0){
+            break;
+        }
+
+        sum += x;
+    }
+
+    // Em HTML, usamos o método innerHTML para exibir a saída
+    document.getElementById('result').innerHTML = "O resultado da soma é " + sum;
+    document.getElementById('result').style.display = 'block';
+}
