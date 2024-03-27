2e10002f38de3d0bdba27270521dcb2a2dda2024
GL-Gabriel-Lincoln <linco.oficial@gmail.com>
2024-03-27 04:40:38 -0300

    .

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let userInput = document.getElementById('myInput').value;
+    
+    await fetch('https://nqi2zih7pd7woez6ah7fu2mjfe0mdfgu.lambda-url.us-east-2.on.aws/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({text: userInput}),
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch((error) => console.error('Error:', error));
 
-fetch('https://nqi2zih7pd7woez6ah7fu2mjfe0mdfgu.lambda-url.us-east-2.on.aws/', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({text: userInput}),
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch((error) => console.error('Error:', error));
+    await escreverNoArquivo(userInput);
