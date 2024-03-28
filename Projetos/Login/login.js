1a98ed73b362f82cbd84db0a4ab191759a6102a0
GL-Gabriel-Lincoln <linco.oficial@gmail.com>
2024-03-28 17:54:51 -0300

    .

--- Projetos/Login/login.js
+++ Projetos/Login/login.js
@@ -9,6 +9,40 @@ document.getElementById('texto2').addEventListener('keydown', function(e) {
         e.preventDefault();
     }
 });
+
+document.getElementById('texto1').addEventListener('keypress', function(e) {
+    var regex = new RegExp("^[a-zA-Z0-9._]*$");
+    var char = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+    if (!regex.test(char) || this.value.length >= 20) {
+        e.preventDefault();
+    }
+});
+
+document.getElementById('texto2').addEventListener('keypress', function(e) {
+    var regex = new RegExp("^[a-zA-Z0-9.*]*$");
+    var char = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+    if (!regex.test(char) || this.value.length >= 30) {
+        e.preventDefault();
+    }
+});
+
+document.getElementById('texto1').addEventListener('paste', function(e) {
+    var pasteData = e.clipboardData.getData('text');
+    var regex = new RegExp("^[a-zA-Z0-9._]*$");
+    if (!regex.test(pasteData) || this.value.length >= 20) {
+        e.preventDefault();
+    }
+});
+
+document.getElementById('texto2').addEventListener('paste', function(e) {
+    var pasteData = e.clipboardData.getData('text');
+    var regex = new RegExp("^[a-zA-Z0-9.*]*$");
+    if (!regex.test(pasteData) || this.value.length >= 30) {
+        e.preventDefault();
+    }
+});
+
+
 document.getElementById('myForm').addEventListener('submit', function(e) {
 e.preventDefault();
 var texto1 = document.getElementById('texto1').value;
@@ -24,6 +58,7 @@ fetch('https://ka4gmdhxnh.execute-api.us-east-2.amazonaws.com/Teste/escrever', {
 .then(response => response.json())
 .then(data => {
     if (data.message) {
+        document.getElementById('mensagem').style.display = 'inline-block';
         document.getElementById('mensagem').textContent = data.message;
     }
 })
