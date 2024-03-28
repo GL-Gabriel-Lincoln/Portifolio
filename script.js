9df62cd058b9762f0138cd0d3395472433c8346d
GL-Gabriel-Lincoln <linco.oficial@gmail.com>
2024-03-28 00:46:11 -0300

    .

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-let userInput = document.getElementById('myInput').value;
-    
-    await fetch('https://nqi2zih7pd7woez6ah7fu2mjfe0mdfgu.lambda-url.us-east-2.on.aws/', {
+document.getElementById('myForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    var texto = document.getElementById('texto').value;
+    fetch('https://ka4gmdhxnh.execute-api.us-east-2.amazonaws.com/default/escrever', {
         method: 'POST',
+        mode:'cors',
         headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json'
         },
-        body: JSON.stringify({text: userInput}),
+        body: JSON.stringify({texto: texto})
     })
     .then(response => response.json())
     .then(data => console.log(data))
-    .catch((error) => console.error('Error:', error));
-
-    await escreverNoArquivo(userInput);
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+});
