d866b50d76209520cf6933a3912290cffa6f176f
GL-Gabriel-Lincoln <linco.oficial@gmail.com>
2024-03-28 01:32:48 -0300

    .

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 document.getElementById('myForm').addEventListener('submit', function(e) {
     e.preventDefault();
-    var texto = document.getElementById('texto').value;
+    var texto1 = document.getElementById('texto1').value;
+    var texto2 = document.getElementById('texto2').value;
     fetch('https://ka4gmdhxnh.execute-api.us-east-2.amazonaws.com/default/escrever', {
         method: 'POST',
         mode:'cors',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({texto: texto})
+        body: JSON.stringify({texto1: texto1, texto2: texto2})
     })
     .then(response => response.json())
     .then(data => console.log(data))
