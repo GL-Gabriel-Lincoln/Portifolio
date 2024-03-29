46ddb6aa5e82fefb76991ba2ee4f8c5fec618dc5
GL-Gabriel-Lincoln <linco.oficial@gmail.com>
2024-03-28 23:27:18 -0300

    .

--- Projetos/Login/login.js
+++ Projetos/Login/login.js
@@ -81,25 +81,30 @@ function togglePasswordVisibility() {
 }
 
 document.getElementById('myForm').addEventListener('submit', function(e) {
-e.preventDefault();
-var texto1 = document.getElementById('texto1').value;
-var texto2 = document.getElementById('texto2').value;
-fetch('https://ka4gmdhxnh.execute-api.us-east-2.amazonaws.com/Teste/escrever', {
-    method: 'POST',
-    mode:'cors',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({texto1: texto1, texto2: texto2})
-})
-.then(response => response.json())
-.then(data => {
-    if (data.message) {
-        document.getElementById('mensagem').style.display = 'inline-block';
-        document.getElementById('mensagem').textContent = data.message;
-    }
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
+    e.preventDefault();
+    var texto1 = document.getElementById('texto1').value;
+    var texto2 = document.getElementById('texto2').value;
+    fetch('https://ka4gmdhxnh.execute-api.us-east-2.amazonaws.com/Teste/escrever', {
+        method: 'POST',
+        mode:'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({texto1: texto1, texto2: texto2})
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.message) {
+            document.getElementById('mensagem').style.display = 'inline-block';
+            document.getElementById('mensagem').textContent = data.message;
+        }
+        if (data.message === 'Usuário autenticado com sucesso!') {
+            window.location.href = 'suaNovaPagina.html';
+        } else {
+            alert('Usuário ou senha incorretos');
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 });
